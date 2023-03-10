a78c7906c216a8a6e04f89670d3f4a0fe346cf46
Julie <juliebarreto897@gmail.com>
2023-03-09 18:21:03 -0800

    test

--- backend/db/seeders/20230307170718-demo-user.js
+++ backend/db/seeders/20230307170718-demo-user.js
@@ -29,7 +29,7 @@ module.exports = {
         lastName: 'Ma3',
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3') //hello
       }
     ], {});
   },
