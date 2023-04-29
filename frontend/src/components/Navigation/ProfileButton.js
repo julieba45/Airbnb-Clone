624a5ca6b67fd53898e07619646d2047fff2ff62
Julie <juliebarreto897@gmail.com>
2023-04-29 16:59:42 -0500

    Redoing all the bonus

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,56 +1,84 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useDispatch } from 'react-redux';
-import * as sessionActions from '../../store/session';
-import './ProfileButton.css';
+// import React, { useState, useEffect, useRef } from "react";
+// import { useDispatch } from 'react-redux';
+// import * as sessionActions from '../../store/session';
+// import OpenModalMenuItem from './OpenModalMenuItem';
+// import LoginFormModal from '../LoginModals/LoginFormModal';
+// import SignupFormModal from '../LoginModals/SignupFormModal';
+// import "./ProfileButton.css";
+// import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
-  const ulRef = useRef();
+// function ProfileButton({ user }) {
+//   const dispatch = useDispatch();
+//   const [showMenu, setShowMenu] = useState(false);
+//   const ulRef = useRef();
+//   const history = useHistory(); //added
 
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
+//   const openMenu = () => {
+//     if (showMenu) return;
+//     setShowMenu(true);
+//   };
 
-  useEffect(() => {
-    if (!showMenu) return;
+//   const goToSignUp = () => {
+//     closeMenu();
+//     history.push('/signup')
+//   }
 
-    const closeMenu = (e) => {
-      if (!ulRef.current.contains(e.target)) {
-        setShowMenu(false);
-        // evaluates to true if click happened inside of the element
-        // evaluates to false if click happened outside of the element
-      }
-    };
+//   useEffect(() => {
+//     if (!showMenu) return;
 
-    document.addEventListener('click', closeMenu);
+//     const closeMenu = (e) => {
+//       if (!ulRef.current.contains(e.target)) {
+//         setShowMenu(false);
+//       }
+//     };
 
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+//     document.addEventListener('click', closeMenu);
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
+//     return () => document.removeEventListener("click", closeMenu);
+//   }, [showMenu]);
 
-  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+//   const closeMenu = () => setShowMenu(false);
 
-  return (
-    <>
-      <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
-      </button>
-      <ul className={ulClassName} ref={ulRef}>
-        <li>{user.username}</li>
-        <li>{user.firstName} {user.lastName}</li>
-        <li>{user.email}</li>
-        <li>
-          <button onClick={logout}>Log Out</button>
-        </li>
-      </ul>
-    </>
-  );
-}
+//   const logout = (e) => {
+//     e.preventDefault();
+//     dispatch(sessionActions.logout());
+//     closeMenu();
+//   };
 
-export default ProfileButton;
+//   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+//   return (
+//     <>
+//       <button onClick={openMenu}>
+//         <i className="fas fa-user-circle" />
+//       </button>
+//       <ul className={ulClassName} ref={ulRef}>
+//         {user ? (
+//           <>
+//             <li>{user.username}</li>
+//             <li>{user.firstName} {user.lastName}</li>
+//             <li>{user.email}</li>
+//             <li>
+//               <button onClick={logout}>Log Out</button>
+//             </li>
+//           </>
+//         ) : (
+//           <>
+//             <OpenModalMenuItem
+//               itemText="Log In"
+//               onItemClick={closeMenu}
+//               modalComponent={<LoginFormModal />}
+//             />
+//             <OpenModalMenuItem
+//               itemText="Sign Up"
+//               onItemClick={closeMenu}
+//               modalComponent={<SignupFormModal />}
+//             />
+//           </>
+//         )}
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default ProfileButton;
