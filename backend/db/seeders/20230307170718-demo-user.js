da88301c8fb892982f5b390c1171f42cad28a346
Julie <juliebarreto897@gmail.com>
2023-05-10 14:26:06 -0500

    reseeding data

--- backend/db/seeders/20230307170718-demo-user.js
+++ backend/db/seeders/20230307170718-demo-user.js
@@ -18,18 +18,32 @@ module.exports = {
         hashedPassword: bcrypt.hashSync('password')
       },
       {
-        firstName: 'Jon1',
-        lastName: 'Ma2',
+        firstName: 'User1',
+        lastName: 'Test1',
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password1')
       },
       {
-        firstName: 'Jon3',
-        lastName: 'Ma3',
+        firstName: 'User2',
+        lastName: 'Test2',
         email: 'user2@user.io',
         username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password2') //hello
+      },
+      {
+        firstName: 'User3',
+        lastName: 'Test3',
+        email: 'user3@user.io',
+        username: 'FakeUser3',
         hashedPassword: bcrypt.hashSync('password3') //hello
+      },
+      {
+        firstName: 'User4',
+        lastName: 'Test4',
+        email: 'user4@user.io',
+        username: 'FakeUser4',
+        hashedPassword: bcrypt.hashSync('password4') //hello
       }
     ], {});
   },
@@ -38,7 +52,7 @@ module.exports = {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2', 'FakeUser3','FakeUser4' ] }
     }, {});
   }
 };
--- backend/db/seeders/20230314042403-spotimages.js
+++ backend/db/seeders/20230314042403-spotimages.js
@@ -13,14 +13,80 @@ module.exports = {
    return queryInterface.bulkInsert(options, [
     {
       spotId: 1,
-      url:'url image',
+      url:'https://a0.muscache.com/im/pictures/c79965b5-9b22-4504-af7b-3131f5c25dfa.jpg?im_w=960',
       preview: true
     },
     {
       spotId: 1,
-      url:'url image',
+      url:'https://a0.muscache.com/im/pictures/a37685ad-4b8f-402d-b9f8-d408e1e9b809.jpg?im_w=1200',
+      preview: false
+    },
+    {
+      spotId: 1,
+      url:'https://a0.muscache.com/im/pictures/ddc400b2-0787-4661-b8bd-7ad3052fabf8.jpg?im_w=1200',
+      preview: false
+    },
+    {
+      spotId: 1,
+      url:'https://a0.muscache.com/im/pictures/1564b1af-125a-40c2-a633-a70f13745896.jpg?im_w=1200',
+      preview: false
+    },
+    {
+      spotId: 1,
+      url:'https://a0.muscache.com/im/pictures/78362fe0-ac0e-47d5-a929-bb3be94c9695.jpg?im_w=1200',
+      preview: false
+    },
+    {
+      spotId: 2,
+      url:'https://a0.muscache.com/im/pictures/miso/Hosting-44274750/original/55ea1f7b-911e-46d9-b35c-aa288d8127dc.jpeg?im_w=960',
       preview: true
-    }
+    },
+    {
+      spotId: 2,
+      url:'https://a0.muscache.com/im/pictures/miso/Hosting-44274750/original/f1474bd5-16a7-4699-b33a-b036f2f37c8e.jpeg?im_w=1200',
+      preview: false
+    },
+    {
+      spotId: 2,
+      url:'https://a0.muscache.com/im/pictures/miso/Hosting-44274750/original/a69c6a71-6955-4e9f-89b1-1954decb5e9c.jpeg?im_w=1200',
+      preview: false
+    },
+    {
+      spotId: 2,
+      url:'https://a0.muscache.com/im/pictures/miso/Hosting-44274750/original/605f3f13-91c2-46f6-8a57-1cd936c55556.jpeg?im_w=1200',
+      preview: false
+    },
+    {
+      spotId: 2,
+      url:'https://a0.muscache.com/im/pictures/miso/Hosting-44274750/original/dcbac177-49ef-4d72-8a52-9a9acd2f06a9.jpeg?im_w=1200',
+      preview: false
+    },
+    {
+      spotId: 3,
+      url:'https://a0.muscache.com/im/pictures/miso/Hosting-20605023/original/0be3f493-fd2d-434e-b557-ad2c189b1543.jpeg?im_w=1440',
+      preview: true
+    },
+    {
+      spotId: 3,
+      url:'https://a0.muscache.com/im/pictures/miso/Hosting-20605023/original/8c7f1364-f2b3-4e88-beec-beb45c0b53b3.jpeg?im_w=1440',
+      preview: false
+    },
+    {
+      spotId: 3,
+      url:'https://a0.muscache.com/im/pictures/miso/Hosting-20605023/original/0017dd02-0ed6-403a-9e65-9c3c6c17e474.jpeg?im_w=1440',
+      preview: false
+    },
+    {
+      spotId: 3,
+      url:'https://a0.muscache.com/im/pictures/miso/Hosting-20605023/original/c9a55179-dfeb-49fd-a68c-a68d57cef831.jpeg?im_w=1440',
+      preview: false
+    },
+    {
+      spotId: 3,
+      url:'https://a0.muscache.com/im/pictures/miso/Hosting-20605023/original/b0f58123-1920-4c0b-93ec-53d07229837b.jpeg?im_w=1440',
+      preview: false
+    },
+
    ])
   },
 
